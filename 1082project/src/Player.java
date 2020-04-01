7006278c7d3779fd8469feac7e2f951b75b3fb6d
tristan <tristan@10.0.0.108>
2020-04-01 18:28:53 -0500

    updating player and game class

--- 1082project/src/Player.java
+++ 1082project/src/Player.java
@@ -1,4 +1,53 @@
 
 public class Player {
+	
+    private int life;
+    private int ammo;
+    private int shield;
+
+
+    public Player(int life, int ammo, int shield) 
+
+    {
+        super();
+        this.life = 3;
+        this.ammo = 0;
+        this.shield = 0;
+    }
+
+
+
+    public int getLife() {
+        return life;
+    }
+
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+
+    public int getAmmo() {
+        return ammo;
+    }
+
+
+    public void setAmmo(int ammo) {
+        this.ammo = ammo;
+    }
+
+
+    public int getShield() {
+        return shield;
+    }
+
+
+    public void setShield(int shield) {
+        this.shield = shield;
+    }
+
+	
+	
+	
 
 }
