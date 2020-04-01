debb497c883f8889f4cc15874fd268bb7fa31130
austinthor <austinthor@10.0.0.173>
2020-04-01 18:24:26 -0500

    Update

--- 1082project/src/Game.java
+++ 1082project/src/Game.java
@@ -1,53 +1,8 @@
 
+import javax.swing.JButton;
+
 public class Game
 {
-	
-	    private int life;
-	    private int ammo;
-	    private int shield;
-
-
-	    public Game(int life, int ammo, int shield) 
-
-	    {
-	        super();
-	        this.life = 3;
-	        this.ammo = 0;
-	        this.shield = 0;
-	    }
-
-
-
-	    public int getLife() {
-	        return life;
-	    }
-
-
-	    public void setLife(int life) {
-	        this.life = life;
-	    }
-
-
-	    public int getAmmo() {
-	        return ammo;
-	    }
-
-
-	    public void setAmmo(int ammo) {
-	        this.ammo = ammo;
-	    }
-
-
-	    public int getShield() {
-	        return shield;
-	    }
-
-
-	    public void setShield(int shield) {
-	        this.shield = shield;
-	    }
-
-
 
 
 	
--- 1082project/src/Player.java
+++ 1082project/src/Player.java
@@ -1,4 +1,54 @@
 
-public class Player {
+public class Player 
+{
 
+    private int life;
+    private int ammo;
+    private int shield;
+
+
+    public Game(int life, int ammo, int shield) 
+
+    {
+        this.life = 3;
+        this.ammo = 0;
+        this.shield = 0;
+    }
+
+
+
+    public int getLife() {
+        return life;
+    }
+
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+
+    public int getAmmo() {
+        return ammo;
+    }
+
+
+    public void setAmmo(int ammo) {
+        this.ammo = ammo;
+    }
+
+
+    public int getShield() {
+        return shield;
+    }
+
+
+    public void setShield(int shield) {
+        this.shield = shield;
+    }
+
+
+    public int shoot()
+    {
+    	
+    }
 }
