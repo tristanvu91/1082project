2f9c10f44d25fda73a7a0a56d091b626bf3e0371
austinthor <austinthor@localhost>
2020-04-04 22:48:09 -0500

    update

--- 1082project/src/Driver.java
+++ 1082project/src/Driver.java
@@ -2,18 +2,10 @@
 public class Driver {
 	
 	public static void main(String[]args) {
+		
+	}
 
 		Game game1 = new Game();
-		Game game2 = new Game();
-		
-<<<<<<< HEAD
-
-		game1.Fight();
 		
 
-=======
->>>>>>> refs/remotes/origin/master
-
-	}
-}
 }
